06932f38fc4a2c920ca5676431604507be63ba93
Nathan Devooght-Ozenne Debaz <nathan@MacBook-Pro-de-Nathan.local>
2022-05-19 10:26:48 +0200

    sign up

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,81 +1,124 @@
-import React, {useEffect, useState} from 'react';
-import './styles.scss';
-import { searchParty } from '../../api/party';
-import { logInAPI, signUpAPI } from '../../api/session';
+import React, { useState } from "react";
+import "./styles.scss";
+import { searchParty } from "../../api/party";
+import { logInAPI, signUpAPI } from "../../api/session";
 
 const Home = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [logIn, setLogIn] = useState<boolean>(false);
+  const [signUp, setSignUp] = useState<boolean>(false);
+  const [createParty, setCreateParty] = useState<boolean>(false);
 
-  const [inputValue, setInputValue] = useState('');
-  const [password , setPassword] = useState('');
-  const [logIn, setLogIn] = useState(false);
-  const [signUp, setSignUp] = useState(false);
+  const handleParty: () => void = async () => {
+    const response: any = await searchParty(inputValue);
+    switch (response.status) {
+      case 404:
+        console.log("Party does not exist");
+        setCreateParty(true);
+        break;
+
+      case 200:
+        console.log(response.data);
+        break;
+
+      case 401:
+        setLogIn(true);
+        setInputValue("");
+        break;
+
+      default:
+        console.error("Error in party get");
+        break;
+    }
+  };
+
+  const handleLogIn: () => void = async () => {
+    const response: any = await logInAPI(inputValue, password);
+    switch (response) {
+      case 202:
+        setLogIn(false);
+        setInputValue("");
+        setPassword("");
+        break;
+      case 404:
+        console.log("User does not exist");
+        setLogIn(false);
+        setSignUp(true);
+        break;
+      case 406:
+        console.log("Wrong password");
+        setSignUp(false);
+        setLogIn(true);
+        break;
+      default:
+        console.error("Error in log in");
+        break;
+    }
+  };
+
+  const handleSignUp: () => void = async () => {
+    const response: any = await signUpAPI(inputValue, password);
+    if (response.status === 201) {
+      await logInAPI(inputValue, password);
+      setSignUp(false);
+      setLogIn(false);
+      setInputValue("");
+      setPassword("");
+    } else {
+      console.log("Error in sign up");
+    }
+  };
 
   return (
     <section className="home">
-      <form id='form-wrap' className="home__wrap" onSubmit={async (event) => {
-        event.preventDefault();
-        if (!logIn) {
-          const response: any = await searchParty(inputValue);
-          switch (response.status) {
-            case 404:
-              console.log('Party does not exist');
-              break;
-
-              case 200:
-              console.log(response.data);
-              break;
-
-              case 401:
-              setLogIn(true);
-              setInputValue('');
-              break;
-
-            default:
-              console.error('Error in party get')
-              break;
+      <form
+        id="form-wrap"
+        className="home__wrap"
+        onSubmit={async (event) => {
+          event.preventDefault();
+          if (!logIn && !signUp) {
+            console.log("Party");
+            handleParty();
+          } else if (logIn) {
+            console.log("Log In");
+            handleLogIn();
+          } else if (signUp) {
+            console.log("Sign Up");
+            handleSignUp();
           }
-        };
-      }}>
-        <input value={inputValue} type="text" name="input" id="input" className='home__wrap__input' onChange={(event) => {
-          setInputValue(event.target.value);
-        }} />
-        { logIn && <input value={password} type="password" name="input" id="password" className='home__wrap__input' onChange={(event) => {
-          setPassword(event.target.value);
-        }} onKeyDown={async (event) => {
-          if (event.key === 'Enter') {
-            const response: any = await logInAPI(inputValue, password);
-            switch (response.status) {
-              case 200:
-                setLogIn(false);
-                setInputValue('');
-                setPassword('');
-                break;
-              case 404:
-                console.log('User does not exist');
-                setSignUp(true);
-                break;
-              case 406:
-                console.log('Wrong password');
-                break;
-              default:
-                console.error('Error in log in');
-                break;
-            };
-          };
-        }} /> }
-        {signUp && <button type="submit" className='home__wrap__submit' onClick={async () => {
-          console.log('Sign up');
-          const response: any = await signUpAPI(inputValue, password);
-          if (response.status === 201) {
-            await logInAPI(inputValue, password);
-            setSignUp(false);
-            setLogIn(false);
-            setInputValue('');
-            setPassword('');
-          } else {
-            console.log('Error in sign up');
-          };
-        }}>Sign Up</button>}
+        }}
+      >
+        <input
+          value={inputValue}
+          type="text"
+          name="input"
+          id="input"
+          className="home__wrap__input"
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+        {(logIn || signUp) && (
+          <input
+            value={password}
+            type="password"
+            name="input"
+            id="password"
+            className="home__wrap__input"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            onFocus={() => handleLogIn()}
+          />
+        )}
+        {(logIn || signUp || createParty) && (
+          <button type="submit" className="home__wrap__submit">
+            {logIn && "Log In"}
+            {signUp && "Sign Up"}
+            {createParty && "Create Party"}
+          </button>
+        )}
       </form>
     </section>
   );
