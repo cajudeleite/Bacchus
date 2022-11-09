2c449c091a2a72799f679fef8d26eae5196d9d0d
Caju <nathandebaz266@gmail.com>
2022-11-09 16:28:57 +0100

    changed opacity for error

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../components/Button";
 
 const Error = ({ text, onClick }: { text: string; onClick?: (() => () => void) | undefined }) => (
-  <div className="flex flex-col space-y-5 text-center text-red-400 opacity-70">
+  <div className="flex flex-col space-y-5 text-center text-red-400 opacity-80">
     <h1 className="severe-lower-case text-8xl">B</h1>
     <p>{text}</p>
     {onClick && <Button text="Try again" onClick={onClick} />}
